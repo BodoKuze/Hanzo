c00889a3dbb3f8e7f4120a159bb96346e8b5cdd9
Piequals31415 <elias.fenten.e@gmail.com>
2023-04-02 13:11:41 +0200

    Arbeit an der Map

--- code/main_game.py
+++ code/main_game.py
@@ -4,7 +4,7 @@ import pygame
 import os
 from player import Player
 from platforms import *
-from enemy import Enemy
+from enemy import *
 
 from background import Background
 from label import Text
@@ -19,13 +19,12 @@ class Game:
         self.p = pygame
         self.player = Player(master,self.p.K_w,self.p.K_s,self.p.K_a,self.p.K_d,self.p.K_SPACE,self.p.K_k,100,100)   
         self.delta_time = 0
-        self.enemy_hitbox_list = [Enemy(master,30,0)]
         self.bg = Background(master)
         self.text = Text(master,self.font_path,30,"60",0,0,100,50,(0,0,0))
         a = Image_Pack(self.master,fr"{os.getcwd()}\sprites\sprite_block.png",8,1,(50,400)).get_images()
         b = Image_Pack(self.master,fr"{os.getcwd()}\sprites\ds_block.png",6,1,(50,300)).get_images()
         
-        self.platforms_hit_box_list = [
+        self.platform_list = [
             Construction(master,0,10,8,1,a,[0]*8),
             movable_Platform(master,8,10,3,1,a,[2]*3,8,2),
             Construction(master,20,10,3,1,a,[1]*3),
@@ -35,6 +34,10 @@ class Game:
             destroy_Platform(master,-3,10,3,1,b,[0]*3,1,4),
             Construction(master,-100,10,97,1,a,[0]*97),
             ]
+        
+        self.enemy_list = [
+            Bat(self.master,0,300)
+        ]
 
     def update_bg(self):
         self.bg.update(self.scroll,self.delta_time)
@@ -67,12 +70,15 @@ class Game:
         self.update_label(clock)
         
     def update_enteties(self,e):
-        self.scroll = self.player.update(self.delta_time,self.platforms_hit_box_list,None,e)
-        #for i in self.enemy_hitbox_list:
-        #    i.update(self.delta_time)        
+        self.scroll = self.player.update(self.delta_time,self.platform_list,None,e)
+        
+        for i in self.enemy_list:
+            i.update(self.delta_time,self.scroll)     
+
+
     def platform_update(self):
         
-        for i in self.platforms_hit_box_list:
+        for i in self.platform_list:
             i.update(self.player,self.scroll)
 
     def update_label(self,clock):
